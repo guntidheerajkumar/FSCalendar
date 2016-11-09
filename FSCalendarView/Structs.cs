896e8790fd0c59655b9af05bdfa485d51a43343c
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2016-11-09 17:34:03 +0530

    Month and Year View Calendar
    Month and Year View Calendar

---
+++ FSCalendarView/Structs.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace FSCalendarView
+{
+	
+	public enum FSCalendarFlow : int
+	{
+		Vertical,
+		Horizontal
+	}
+
+	
+	public enum FSCalendarCellShape : int
+	{
+		Circle = 0,
+		Rectangle = 1
+	}
+
+	
+	public enum FSCalendarUnit : int
+	{
+		Month,
+		WeekOfYear,
+		Day
+	}
+
+	
+	public enum FSCalendarCellState : int
+	{
+		Normal = 0,
+		Selected = 1,
+		Placeholder = 1 << 1,
+		Disabled = 1 << 2,
+		Today = 1 << 3,
+		Weekend = 1 << 4,
+		TodaySelected = Today | Selected
+	}
+
+	
+	public enum FSCalendarCaseOptions : int
+	{
+		HeaderUsesDefaultCase = 0,
+		HeaderUsesUpperCase = 1,
+		WeekdayUsesDefaultCase = 0 << 4,
+		WeekdayUsesUpperCase = 1 << 4,
+		WeekdayUsesSingleUpperCase = 2 << 4
+	}
+
+	
+	public enum FSCalendarSeparators : int
+	{
+		None = 0,
+		InterRows = 1 << 0,
+		InterColumns = 1 << 1,
+		BelowWeekdays = 1 << 2
+	}
+
+	
+	public enum FSCalendarScope : int
+	{
+		Month,
+		Week
+	}
+
+	
+	public enum FSCalendarScrollDirection : int
+	{
+		Vertical,
+		Horizontal
+	}
+
+	
+	public enum FSCalendarPlaceholderType : int
+	{
+		None = 0,
+		FillHeadTail = 1,
+		FillSixRows = 2
+	}
+
+	
+	public enum FSCalendarMonthPosition : int
+	{
+		Previous,
+		Current,
+		Next,
+		OutOfBounds,
+		NotFound
+	}
+
+	
+	public enum FSCalendarTransition : int
+	{
+		None,
+		MonthToWeek,
+		WeekToMonth
+	}
+
+	
+	public enum FSCalendarTransitionState : int
+	{
+		dle,
+		nProgress
+	}
+}
---
+++ SampleCalendar/ViewController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Foundation;
+using CoreGraphics;
+using FSCalendarView;
+using UIKit;
+
+namespace SampleCalendar
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			var calendar = new FSCalendar();
+			calendar.DataSource = new CalendarDataSource();
+			calendar.Delegate = new CalendarDelegate();
+			calendar.Frame = new CGRect(0, 50, this.View.Bounds.Width, 300);
+			this.View.AddSubview(calendar);
+			calendar.ScopeGesture.Enabled = true;
+			calendar.ReloadData();
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+		}
+	}
+
+	public class CalendarDelegate : FSCalendarDelegate
+	{
+		public override void Calendar(FSCalendar calendar, CGRect bounds, bool animated)
+		{
+			var calendarFrame = calendar.Frame;
+			calendarFrame.Size = bounds.Size;
+			calendar.Frame = calendarFrame;
+		}
+
+		public override void DidSelectDate(FSCalendar calendar, NSDate date)
+		{
+			Console.WriteLine(date);
+		}
+	}
+
+	public class CalendarDataSource : FSCalendarDataSource
+	{
+		public override UIImage ImageForDate(FSCalendar calendar, NSDate date)
+		{
+			DateTime reference = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(2001, 1, 1, 0, 0, 0));
+			var d = reference.AddSeconds(date.SecondsSinceReferenceDate);
+			if (d.Date == new DateTime(2016, 11, 17)) {
+				return UIImage.FromBundle("cake");
+			}
+			return new UIImage();
+		}
+	}
+}
